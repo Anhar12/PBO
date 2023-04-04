41f1df67e239d18ee54824296ba5d13632e60b8e
Anhar Khoirun Najib <92861249+Anhar12@users.noreply.github.com>
2023-04-05 02:28:16 +0800

    Add files via upload

---
+++ POSTTEST5/src/Game.java
@@ -0,0 +1,31 @@
+public abstract class Game {
+    // deklarasi field
+    protected String Game, mataUang;
+
+    // constructor
+    public Game (String nama_game, String matauang_game){
+        this.Game = nama_game;
+        this.mataUang = matauang_game;
+    }
+
+    // method setter getter
+    public String getGame() {
+        return this.Game;
+    }
+
+    public void setGame(String Game) {
+        this.Game = Game;
+    }
+
+    public String getMataUang() {
+        return this.mataUang;
+    }
+    
+    public void setMataUang(String mataUang) {
+        this.mataUang = mataUang;
+    }
+    
+    // method abstract yang akan di override
+    public abstract void display();
+
+}
